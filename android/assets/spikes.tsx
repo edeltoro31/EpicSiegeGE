ae0274f6ff219d4d6e3eea015e7c8c3455e79983
jaguardado123 <jaguardado123@users.noreply.github.com>
2018-04-19 15:08:23 -0500

    added to map and collison detection
    //added some small features to our map and moved the collison detection a bit further from our avatar so he doesn't stager when he collides with the dragon balls.

---
+++ android/assets/spikes.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="spikes" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="6" columns="3">
+ <image source="Spikes.png" width="51" height="48"/>
+</tileset>
